c3334e369808d9d8b11cddb2dfda510a00b0a402
Alejandro <alejandro@alejandroc.com>
2021-04-06 11:57:04 -0400

    updated comments

--- Web-Widget/Hide-Zendesk-Branding/index.js
+++ Web-Widget/Hide-Zendesk-Branding/index.js
@@ -4,7 +4,7 @@
 <script type="text/javascript">
   // on-click of the web widget the following code will run
   zE('webWidget:on', 'open', function() {
-    //creates css
+    // creates any CSS to be added to the Web Widget
     var style = document.createElement('style');
       style.innerHTML = `
         /* hides the zendesk branding */
@@ -12,7 +12,7 @@
           visibility: hidden;
         }
         `;
-  //appends the css in the web widget
+    // appends the CSS in the web widget
     parent.document.getElementById('webWidget').contentDocument.head.appendChild(style);
   });
 </script>
